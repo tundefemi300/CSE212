3730aff56d28335902098ca026df605d4f4a9262
Akanbi Oluwafemi <tundefemi300@gmail.com>
2026-09-25 11:12:19 +0100

    Add earthquake description generation logic
    Implement logic to create earthquake descriptions from features.

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -155,6 +155,13 @@ return true;
         // on those classes so that the call to Deserialize above works properly.
         // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
         // 3. Return an array of these string descriptions.
-        return [];
+        return []; var earthquakes = new List<string>();
+
+foreach (var feature in featureCollection.Features)
+{
+    earthquakes.Add($"{feature.Properties.Place} - Mag {feature.Properties.Mag}");
+}
+
+return earthquakes.ToArray();
     }
 }
